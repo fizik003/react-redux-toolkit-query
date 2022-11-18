1a1121f929d8938eac03779dc591b90480abca6f
yury <kassik0boks@gmail.com>
2022-11-18 12:36:49 +0300

    added all files

---
+++ src/hooks/debounce.ts
@@ -0,0 +1,11 @@
+import { useEffect, useState } from "react";
+
+export function useDebounce(value: string, delay = 300) {
+  const [debounced, setDebounced] = useState(value);
+  useEffect(() => {
+    const handler = setTimeout(() => setDebounced(value), delay);
+    return () => clearTimeout(handler);
+  }, [delay, value]);
+
+  return debounced;
+}
